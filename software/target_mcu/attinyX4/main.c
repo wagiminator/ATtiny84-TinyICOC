7fb818ed9c4896fa37847df2aa18384dda347e02
wagiminator <wagiminator@web.de>
2021-05-01 20:08:41 +0200

    Update firmware

--- software/target_mcu/attinyX4/main.c
+++ software/target_mcu/attinyX4/main.c
@@ -5,6 +5,6 @@ int main(void) {
   OSCCAL = eeprom_read_byte(0);   // Set OSCCAL from EEPROM
   DDRA   = 0b01000000;            // Set PA6 as output
   TCCR1A = 0b01000000;            // Toggle PA6 on compare match
-  TCCR0B = 0b00001001;            // Start timer CTC mode, no prescaler
+  TCCR1B = 0b00001001;            // Start timer CTC mode, no prescaler
   while(1);                       // That's all
 }
